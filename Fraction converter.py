4af8c673ef85cdf89b824dbb76e7d94fd455ed25
Niranchan-L <119785654+Niranchan-L@users.noreply.github.com>
2022-12-07 23:13:49 +0530

    Update Fraction converter.py

--- Fraction converter.py
+++ Fraction converter.py
@@ -1,18 +1,18 @@
 n = int(input("Numerator: "))
 d = int(input("Denominator: "))
 soln = [n,d]
-printed = False # Added
+printed = False
 for x in range(2,max(n,d)):
     if n%d == 0:
         print()
         print((int(n/d)))
-        printed = True # Added
+        printed = True
         break
     else:
         if [n%x,d%x] == [0,0]:
             soln.append(n//x)
             soln.append(d//x)
-if not printed: # Added
+if not printed: 
     print()
     print(soln[-2], "/", soln[-1])
 
